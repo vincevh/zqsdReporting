9c7eec80695f37dc7f1b7c7f7885d984a99500ff
B3nZ3n <B3nZ3n@gmail.com>
2017-06-23 01:27:20 +1200

    new global.R: messages are now loaded even before the 1st page display

--- server.R
+++ server.R
@@ -1,34 +1,3 @@
-source(file = "ETL.R", encoding = "utf-8")
-Sys.setlocale("LC_ALL", locale = "French_Belgium.1252")
-
-library(shiny)
-library(ggplot2)
-library(lubridate)
-library(wordcloud)
-library(stringr)
-library(tm)
-library(SnowballC)
-library(RColorBrewer)
-library(pander)
-library(plotly)
-library(googleVis)
-
-
-yeartoload <- year(Sys.Date())
-messages <- load.msgs(yeartoload)
-scoreshgt <- load.hgtscores(yeartoload)
-
-usercolors <- load.usercolors()
-jColors <- usercolors$color
-names(jColors) <- usercolors$unick
-
-mondayWeekMinus1 <- floor_date(Sys.Date() - 7, "week") + 1
-sundayWeekMinus1 <- floor_date(Sys.Date(), "week")
-
-msgWEEK <-  messages[messages$datetime >= mondayWeekMinus1 &
-             messages$datetime <=  sundayWeekMinus1 , ]
-
-
 
 
 
