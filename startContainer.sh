0cd3636cc34f8b8da720ebfebb7dd85bd8a5d493
B3nZ3n <B3nZ3n@gmail.com>
2017-08-07 15:42:00 +1200

    minor

--- startContainer.sh
+++ startContainer.sh
@@ -1,9 +1,9 @@
 cd ~/zqsdReporting
 docker stop zqsdreporting
+docker stop mynginx
 git pull
 docker build -t zqsdreporting .
 docker run -d --rm -p 1337:1337 --name zqsdreporting -h zqsdreporting zqsdreporting
-docker stop mynginx
 cd ~/zqsdReporting/nginx
 docker build -t mynginx .
 docker run -d -p 80:80 --name mynginx --rm -v ~/myLoc:/usr/share/nginx/html/myLoc --link zqsdreporting:zqsdreporting mynginx
