44b2d5bd1a6d2bd8a5c685d555730ed9578c7c32
B3nZ3n <B3nZ3n@gmail.com>
2017-08-07 02:59:00 +0200

    new change nicks

--- runRepApp.sh
+++ runRepApp.sh
@@ -1,4 +1,5 @@
 #
+##export LANG=fr_BE.utf8
 killall R
-git pull
-R -e "shiny::runApp('.',host= '127.0.0.1', port = 1337)"
+git -C /home/ec2-user/zqsdReporting/ pull
+R -e "shiny::runApp('/home/ec2-user/zqsdReporting/',host= '127.0.0.1', port = 1337)"
