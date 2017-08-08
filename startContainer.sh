f2b7088e92ccc85081c063ef3730e4bd090cb16c
B3nZ3n <B3nZ3n@gmail.com>
2017-08-08 05:22:12 +0200

    nginx again

--- startContainer.sh
+++ startContainer.sh
@@ -4,3 +4,7 @@ docker stop mynginx
 git pull
 docker build -t zqsdreporting .
 docker run -d --rm -p 1337:1337 --name zqsdreporting -h zqsdreporting zqsdreporting
+cd ~/zqsdReporting/nginx
+docker build -t mynginx .
+docker run -d -p 80:80 --name mynginx --rm -v ~/myLoc:/usr/share/nginx/html/myLoc --link zqsdreporting:zqsdreporting mynginx
+
