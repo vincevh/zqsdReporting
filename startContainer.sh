1eb0352553c1fb9db4df36bbf67175a38f2108f3
B3nZ3n <B3nZ3n@gmail.com>
2017-08-15 23:50:42 +1200

    minor

--- startContainer.sh
+++ startContainer.sh
@@ -4,11 +4,11 @@ docker stop slackapi
 git pull
 cd shinyapp
 docker build -t zqsdreporting .
-docker run -d --rm -p 1337:1337 --name zqsdreporting -h zqsdreporting zqsdreporting
 cd ../slackapi
 docker build -t slackapi .
-docker run -d --rm -p 8000:8000 --name slackapi -h slackapi slackapi
 cd ../nginx
 docker build -t mynginx .
-docker run -d -p 80:80 --name mynginx --rm -v ../myLoc:/usr/share/nginx/html/myLoc --link zqsdreporting:zqsdreporting  --link slackapi:slackapi mynginx
+docker run -d --rm -p 1337:1337 --name zqsdreporting -h zqsdreporting zqsdreporting
+docker run -d --rm -p 8000:8000 --name slackapi -h slackapi slackapi
+docker run -d --rm -p 80:80 --name mynginx -v ~/myLoc:/usr/share/nginx/html/myLoc --link zqsdreporting:zqsdreporting  --link slackapi:slackapi mynginx
 
