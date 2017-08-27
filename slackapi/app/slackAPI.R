29fcd24688e9b799deb5b26b078facd29a4d58e5
B3nZ3n <B3nZ3n@gmail.com>
2017-08-27 04:40:20 +0200

    new api

--- slackapi/app/slackAPI.R
+++ slackapi/app/slackAPI.R
@@ -1,12 +1,26 @@
 #' @post /slackAPI
-function(text){
-cat("RECIEVED MSG \n")
-cat(text)
 
-temp <- data.frame(paste("you said" ,text, " "))
-names(temp)<- "text"
+function(text,res){
+  cat("RECIEVED MSG \n")
+  cat("text=")
+  cat(text)
+  cat("\n end text \n")
+  
+  request <- strsplit(text, "+",fixed = TRUE)[[1]]
+  
+  command <- request[2]
+  
+  if ( command == "echo") {
+    toReturn <- data.frame(paste("you said" ,request[3], " "))
+  } else if ( command == "pushtotaunt") {
+    toReturn <- data.frame("smb")
+  } else if ( command == "useless") {
+    toReturn <- data.frame("Most useless of the week is: xxx")
+  } else
+    toReturn <- data.frame("Command not found")
+  
 
-temp
-
+  names(toReturn)<- "text"
+  res$body <- jsonlite::unbox(toReturn)
 }
 
