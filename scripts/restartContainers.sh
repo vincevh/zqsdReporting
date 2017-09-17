51a7ed658ea3b35915014a35464c7415795dfec6
B3nZ3n <B3nZ3n@gmail.com>
2017-09-17 17:48:38 +1200

    final

--- scripts/restartContainers.sh
+++ scripts/restartContainers.sh
@@ -18,4 +18,4 @@ docker run -d --rm -p 3000:3000 --name srfollowup -h srfollowup --link mymysql:m
 sleep 30
 docker run -d --rm -p 8000:8000 --name slackapi -h slackapi --link mymysql:mymysql slackapi
 #docker run  -p 3000:3000 --name srfollowup -h srfollowup --link mymysql:mymysql -v $(pwd)/srfollowup/app:/app -ti srfollowup bash
-docker run -d --rm -p 80:80 --name mynginx -v ../../myLoc:/usr/share/nginx/html/myLoc --link zqsdreporting:zqsdreporting  --link slackapi:slackapi --link srfollowup:srfollowup mynginx
+docker run -d --rm -p 80:80 --name mynginx -v ~/myLoc:/usr/share/nginx/html/myLoc --link zqsdreporting:zqsdreporting  --link slackapi:slackapi --link srfollowup:srfollowup mynginx
